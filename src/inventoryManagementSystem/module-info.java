ac6d96aa505628e6fdf0104d7e06bd879f453f02
Shivani barya <153480939+shivanibarya@users.noreply.github.com>
2024-11-30 10:50:40 +0530

    Update module-info.java

--- src/inventoryManagementSystem/module-info.java
+++ src/inventoryManagementSystem/module-info.java
@@ -1,12 +1,8 @@
-/**
+**
  * Module declaration for the ShadowFox project.
  */
 module ShadowFox {
     // Require the JUnit module for unit testing
     requires org.junit.jupiter.api; // For JUnit 5 (Jupiter)
     requires org.junit.jupiter.engine; // Required for the JUnit 5 engine
-
-    // Export your packages
-    exports ContactManagementSystem; // Exporting ContactManagementSystem package
-    exports calculator; // Exporting calculator package
 }
